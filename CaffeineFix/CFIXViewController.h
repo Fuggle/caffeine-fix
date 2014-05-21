390bf3c4adacf7c2bb37a851190c05dd535c71d9
Jiewei <jiewei24@gmail.com>
2014-05-22 09:03:25 +1000

    Move chart to dashboard

---
+++ CaffeineFix/CFIXViewController.h
@@ -0,0 +1,13 @@
+//
+//  CFIXViewController.h
+//  CaffeineFix
+//
+//  Created by Huang Jiewei on 22/05/14.
+//  Copyright (c) 2014 ___COFFEENINE___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFIXViewController : UIViewController
+
+@end
