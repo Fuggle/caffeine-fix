d3970b890d1363984e4ca0af14bd9fcb606b2877
Jiewei <jiewei24@gmail.com>
2014-05-29 10:49:53 +1000

    Added graph to iPad storyboard

---
+++ CFIXViewControlleriPad.h
@@ -0,0 +1,13 @@
+//
+//  CFIXViewControlleriPad.h
+//  CaffeineFix
+//
+//  Created by Huang Jiewei on 29/05/14.
+//  Copyright (c) 2014 ___COFFEENINE___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFIXViewControlleriPad : UIViewController
+
+@end
